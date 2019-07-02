501b8f1e90ad53a8cafcab29000c4750a8212698
ggera02 <51716779+ggera02@users.noreply.github.com>
2019-07-02 19:59:38 +0200

    Update SicurezzaBot.py

--- SicurezzaBot.py
+++ SicurezzaBot.py
@@ -70,3 +70,76 @@ driver.find_element_by_css_selector('input.cb-test:nth-child(5)').click()
 
 lezione_9 = [ '11', '12.01', '12.02', '12.03', '13', '14']
 for n in lezione_9:
+    driver.get(url_video + n) 
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(400)
+
+driver.get('https://web.spaggiari.eu/col/app/default/corso.php?view=tst&video=01.14')
+time.sleep(5)
+driver.find_element_by_css_selector('input.cb-test:nth-child(11)').click()
+
+lezione_10 = ['15', '16', '17']
+for n in lezione_9:
+    driver.get(url_video + n)
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(400)
+
+lezione_4 = ['18']
+for n in lezione_4:
+    driver.get(url_video + n)
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(1000)
+
+driver.get('https://web.spaggiari.eu/col/app/default/corso.php?view=tst&video=01.18')
+time.sleep(5)
+driver.find_element_by_css_selector('input.cb-test:nth-child(8)').click()
+
+lezione_5 = ['19']
+for n in lezione_5:
+    driver.get(url_video + n)
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(160)
+
+lezione_6 = ['20', '21', '22', '23', '24']
+for n in lezione_6:
+    driver.get(url_video + n)
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(630)
+
+lezione_7 = ['25']
+for n in lezione_6:
+    driver.get(url_video + n)
+    time.sleep(5)
+    play_icon = driver.find_element_by_css_selector('html')
+    play_icon.click()
+    time.sleep(200)
+
+# raggiunge il numero minimo di presenza online
+time.sleep(300)
+
+# risponde al test finale
+time.sleep(15)
+driver.get('https://web.spaggiari.eu/col/app/default/corso.php?view=tsf')
+
+driver.find_element_by_css_selector('div.test-item:nth-child(5) > div:nth-child(1) > div:nth-child(2) > input:nth-child(5)')
+driver.find_element_by_css_selector('div.test-item:nth-child(6) > div:nth-child(1) > div:nth-child(2) > input:nth-child(5)')
+driver.find_element_by_css_selector('div.test-item:nth-child(7) > div:nth-child(1) > div:nth-child(2) > input:nth-child(11)')
+driver.find_element_by_css_selector('div.test-item:nth-child(8) > div:nth-child(1) > div:nth-child(2) > input:nth-child(5)')
+driver.find_element_by_css_selector('div.test-item:nth-child(9) > div:nth-child(1) > div:nth-child(2) > input:nth-child(11)')
+driver.find_element_by_css_selector('div.test-item:nth-child(10) > div:nth-child(1) > div:nth-child(2) > input:nth-child(5)')
+driver.find_element_by_css_selector('div.test-item:nth-child(11) > div:nth-child(1) > div:nth-child(2) > input:nth-child(11)')
+driver.find_element_by_css_selector('div.test-item:nth-child(12) > div:nth-child(1) > div:nth-child(2) > input:nth-child(11)')
+driver.find_element_by_css_selector('div.test-item:nth-child(13) > div:nth-child(1) > div:nth-child(2) > input:nth-child(8))')
+driver.find_element_by_css_selector('div.test-item:nth-child(14) > div:nth-child(1) > div:nth-child(2) > input:nth-child(8)')
+
+print('Hai finito congratulazioni :)')
